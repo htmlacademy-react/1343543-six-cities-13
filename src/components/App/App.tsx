bd4761802929b917ecc1554f1670dcf847673638
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-08-02 12:24:14 +0300

    Добавляет маршруты

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,9 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../../pages/main/Main';
-// import Offer from '../../pages/offer/Offer';
-// import Login from '../../pages/login/Login';
-// import Favorites from '../../pages/favorites/Favorites';
+import Offer from '../../pages/offer/Offer';
+import Login from '../../pages/login/Login';
+import Favorites from '../../pages/favorites/Favorites';
+import NotFound from '../../pages/not-found/Not-Found';
 
 type AppQunatityProps = {
   quantity: number;
@@ -9,7 +11,19 @@ type AppQunatityProps = {
 
 function App({quantity}: AppQunatityProps): JSX.Element {
   return (
-    <Main quantity={quantity}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main quantity={quantity}/>} />
+        <Route path="/offer">
+          <Route index element={<div>No page is selected.</div>}></Route>
+          <Route path=":id" element={<Offer />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    
     // <Offer />
     // <Login />
     // <Favorites />
