0dd8a7b291911b521c3fa7e9380cade2186837b9
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-08-05 13:29:45 +0300

    Добавляет форму

---
+++ src/components/AddReview/AddReview.tsx
@@ -0,0 +1,135 @@
+import { useState } from "react";
+
+function AddReview() {
+  const [currentRadio, setRadio] = useState(3);
+  const [reviewText, setReviewText] = useState("");
+
+  const handleRadio = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRadio(Number(e.target.value)); 
+  }
+
+  return (
+    <form className="reviews__form form" action="#" method="post">
+      <label className="reviews__label form__label" htmlFor="review">
+        Your review
+      </label>
+      <div className="reviews__rating-form form__rating">
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value={5}
+          checked={currentRadio === 5}
+          id="5-stars"
+          type="radio"
+          onChange={handleRadio}
+        />
+        <label
+          htmlFor="5-stars"
+          className="reviews__rating-label form__rating-label"
+          title="perfect"
+        >
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value={4}
+          checked={currentRadio === 4}
+          onChange={handleRadio}
+          id="4-stars"
+          type="radio"
+        />
+        <label
+          htmlFor="4-stars"
+          className="reviews__rating-label form__rating-label"
+          title="good"
+        >
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value={3}
+          checked={currentRadio === 3}
+          onChange={handleRadio}
+          id="3-stars"
+          type="radio"
+        />
+        <label
+          htmlFor="3-stars"
+          className="reviews__rating-label form__rating-label"
+          title="not bad"
+        >
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value={2}
+          checked={currentRadio === 2}
+          onChange={handleRadio}
+          id="2-stars"
+          type="radio"
+        />
+        <label
+          htmlFor="2-stars"
+          className="reviews__rating-label form__rating-label"
+          title="badly"
+        >
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value={1}
+          checked={currentRadio === 1}
+          onChange={handleRadio}
+          id="1-star"
+          type="radio"
+        />
+        <label
+          htmlFor="1-star"
+          className="reviews__rating-label form__rating-label"
+          title="terribly"
+        >
+          <svg className="form__star-image" width={37} height={33}>
+            <use xlinkHref="#icon-star" />
+          </svg>
+        </label>
+      </div>
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        onChange={(e) => setReviewText(e.target.value)}
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={reviewText}
+      />
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
+          To submit review please make sure to set{' '}
+          <span className="reviews__star">rating</span> and describe
+          your stay with at least{' '}
+          <b className="reviews__text-amount">50 characters</b>.
+        </p>
+        <button
+          className="reviews__submit form__submit button"
+          type="submit"
+          disabled
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default AddReview
