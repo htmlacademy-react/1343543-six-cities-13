9ede5f36ad7ebd314103ca2f9a6df741f5b6bb84
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-08-09 12:04:25 +0300

    Выносит фильтр в отдельный компонент и добавляет hover

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -7,7 +7,7 @@ import LocationList from '../../components/LocationsList/LocationList';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { setCity } from '../../store/action';
-
+import Filter from '../../components/Filter/Filter';
 
 function Main(): JSX.Element {
   const selectedCity = useAppSelector((state) => state.selectedCity);
@@ -46,32 +46,7 @@ function Main(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offersByCity.length} places to stay in {selectedCity}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <Filter />
               <CardList offers={offersByCity} page={'main'}/>
             </section>
             <div className="cities__right-section">
